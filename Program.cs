24bd3a5c3829c40a4f02adb214530c6ac5c48255
Ilham <pilham271@gmail.com>
2022-03-23 22:54:47 +0700

    tugas taxi app

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Taxii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            taxi Taxi = new taxi();
+            Taxi.drivername = "Agus";
+            Taxi.onduty = true;
+            Taxi.numpassenger = 10;
+
+            Taxi.taxiinfo();
+            Taxi.pickuppassenger();
+            Taxi.droppofpassenger();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Taxii
+{
+    class taxi
+    {
+        public string drivername { get; set; }
+        public bool onduty { get; set; }
+        public int numpassenger { get; set; }
+
+        public void taxiinfo()
+        {
+            string duty;
+
+            if(onduty)
+            {
+                duty = "yes";
+            }
+            else
+            {
+                duty = "no";
+            }
+
+            Console.WriteLine("Driver Name : {0}", drivername);
+            Console.WriteLine("On Duty : {0}", duty);
+            Console.WriteLine("Number of Passenger : {0}", numpassenger);
+        }
+        public void pickuppassenger()
+        {
+            Console.WriteLine("{0} Sedang Menjemput Penumpang ", drivername);
+        }
+        public void droppofpassenger()
+        {
+            Console.WriteLine("{0} Selesai Mengantar Penumpang ", drivername);
+        }
+    }
+}
